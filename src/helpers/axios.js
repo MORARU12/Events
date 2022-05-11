029e9df0adaa36e68533a4415c5f8e8fe655b4a8
Moraru <tiltiobusiness@gmail.com>
2022-05-11 19:55:58 +0300

    gg

--- src/helpers/axios.js
+++ src/helpers/axios.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
-const baseURL = process.env.REACT_APP_BACKEND_URL;
+// const baseURL = process.env.REACT_APP_BACKEND_URL;
+
+const baseURL = "https://api.oginvites.party";
 
 const token = localStorage.getItem("token");
 
